0aedfd34e29be767644771a656196b2ff494f5f6
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-09 11:12:03 +0530

    Initial Commit

---
+++ GeometryReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReader
+//
+//  Created by Heshan Yodagama on 2/9/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
