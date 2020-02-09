06f645278ae263dd54e2fd7584967b9ff462bdc2
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-09 13:04:26 +0530

    geometryReader introduce

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            .border(Color.black)
+            MyRectangle()
+            .border(Color.black)
+        }
+        .frame(width: 150, height: 100)
+        .border(Color.black)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MyRectangle: View {
+    var body: some View {
+        GeometryReader { (geometry) in
+
+            Rectangle()
+                .path(in: CGRect(x: geometry.size.width + 5, y: 0, width: geometry.size.width/2, height: geometry.size.height/2))
+                .fill(Color.blue)
+        }
+        
     }
 }
